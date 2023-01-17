a6513b844924e97722e59e53161d702b6d373cb7
andrewhosack <49813742+andrewhosack@users.noreply.github.com>
2023-01-16 20:20:22 -0500

    Update ICSVService.cs

--- AndrewHosackServiceChannel.Data/Services/ICSVService.cs
+++ AndrewHosackServiceChannel.Data/Services/ICSVService.cs
@@ -6,8 +6,6 @@ namespace AndrewHosackServiceChannel.Data.Services
     {
         public List<CovidCsvModel> ReadCsvForCounty(string county);
         public List<CovidCsvModel> ReadCsvForState(string state);
-        //public bool ValidateCountyNameInCsv(string county);
-        //public bool ValidateStateNameInCsv(string state);
         public string ValidateLocationInCsv(string location);
         public CovidStatistics CollectCovidStatistics(string locationType, string location, DateTime? startDate, DateTime? endDate);
         public CovidDailyStatistics CollectCovidTotalAndNewCases(string locationType, string location, DateTime? startDate, DateTime? endDate);
